baa93a09269cff4ae6918264a3d8da3441e182ad
Makarov96 <estev97@gmail.com>
2023-04-22 12:17:57 -0600

    Initial Commit

---
+++ SpotifyClone/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SpotifyClone
+//
+//  Created by Guerin Steven Colocho Chacon on 22/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpotifyClone/SpotifyCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyCloneApp.swift
+//  SpotifyClone
+//
+//  Created by Guerin Steven Colocho Chacon on 22/04/23.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
